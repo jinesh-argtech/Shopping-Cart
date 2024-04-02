d0e5d7101a9ca706b2f3b60b7215f612fcca5562
jinesh-argtech <jinesh@argtechnologies.in>
2024-04-02 17:45:19 +0530

    wrote the api for login and signup at the backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(
 	cors({
@@ -15,10 +16,13 @@ const database = require("./config/database");
 
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
+const userRoutes = require("./routes/user");
+const dotenv = require("dotenv");
 
 database.connect();
 
 app.use(express.json());
+app.use(cookieParser());
 const fileUpload= require("express-fileupload");
 app.use(fileUpload({
     useTempFiles : true,
@@ -27,6 +31,7 @@ app.use(fileUpload({
 
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/auth", userRoutes);
 
 
 app.get("/", (req, res) => {
